a4f41794ab4b63fbf38105ac3761735c21a31f61
Bhoumik Patidar <bpatidar@qti.qualcomm.com>
2026-09-15 19:01:16 +0530

    Register i386 target and driver flavor
    Keep X86 as a single target family and register i386 target alongside x86_64. Recognize the
    standard elf_i386 linker emulation and route it through the i386 driver flavor.Organize the
    X86 lit tests by family and architecture, and add a driver-recognition test.
    
    Signed-off-by: Bhoumik Patidar <bpatidar@qti.qualcomm.com>

--- lib/Target/X86/TargetInfo/X86TargetInfo.cpp
+++ lib/Target/X86/TargetInfo/X86TargetInfo.cpp
@@ -1,4 +1,4 @@
-//===- x86_64TargetInfo.cpp------------------------------------------------===//
+//===- X86TargetInfo.cpp----------------------------------------------------===//
 // Part of the eld Project, under the BSD License
 // See https://github.com/qualcomm/eld/LICENSE.txt for license information.
 // SPDX-License-Identifier: BSD-3-Clause
@@ -11,9 +11,12 @@
 namespace eld {
 
 eld::Target Thex86_64Target;
+eld::Target TheX86_32Target;
 
 extern "C" void ELDInitializeX86LDTargetInfo() {
   // register into eld::TargetRegistry
+  eld::RegisterTarget X86_32(TheX86_32Target, "i386", llvm::ELF::EM_386,
+                             false);
   eld::RegisterTarget X(Thex86_64Target, "x86_64", llvm::ELF::EM_X86_64, true);
 }
 
